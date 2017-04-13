8b1fa36669a4f3cff3ce1858ea8da03538f87244
Robert Schneiderman <robert.a.schneiderman@gmail.com>
2017-04-12 22:35:17 -0700

    fixed history arrangement to be not blindly ordered, but ordered on calendar by history.day

--- frontend/src/pages/calendar/components/Week.jsx
+++ frontend/src/pages/calendar/components/Week.jsx
@@ -10,40 +10,38 @@ class Week extends Component {
     }
 
     returnDay(history, i) {
-        let { activeWeek, taskDict, tsDict, dispatches } = this.props;
-        let startOfWeek = moment().subtract((activeWeek) * 7, 'days').startOf('week').add(1, 'days');
-        let date = startOfWeek.add(i, 'days');
-        return <Day 
-                date={date}
-                history={history}
-                taskDict={taskDict}
-                tsDict={tsDict}
-                dispatches={dispatches}
-                key={`tsd-${i}`} />;
     }  
 
-    renderIncompleteWeek(historys) {
-        let days = [];
-        let j = 0;
-
-        for (let i = 0; i <= 6; i++) {
-            let history = historys[j];
-            (history && history.day === i) ? j++ : history = {tasks: [], timestamps: []};
-            days.push(this.returnDay(history, i));
-        }      
-        return days;
-    }
-
-    renderCompleteWeek(historys) {
-        return historys.map((history, i) => this.returnDay(history, i));
+    arrangeHistorys() {
+        let { week } = this.props;
+        let arrangedHistorys = [{tasks: [], timestamps:[]}, {tasks: [], timestamps:[]}, {tasks: [], timestamps:[]}, {tasks: [], timestamps:[]}, {tasks: [], timestamps:[]}, {tasks: [], timestamps:[]}, {tasks: [], timestamps:[]}];
+        week.forEach(history => {
+            arrangedHistorys[history.day] = history;
+        });
+        // debugger;
+        return arrangedHistorys;
     }
 
     renderWeek() {
-        let { week, historyDict } = this.props;
+        let { week, historyDict, activeWeek, taskDict, tsDict, dispatches } = this.props;
         // let historys = week.map(histId => historyDict[histId]);
         // debugger;
         // historys = this.splitTimestamps(historys);
-        return (week.length < 7) ? this.renderIncompleteWeek(week) : this.renderCompleteWeek(week);
+        let startOfWeek = moment().subtract((activeWeek) * 7, 'days').startOf('week').add(1, 'days');
+        let historys = this.arrangeHistorys();
+
+        return historys.map((history, i) => {
+            let date = startOfWeek.add(i, 'days');
+
+            return <Day 
+                    date={date}
+                    history={history}
+                    taskDict={taskDict}
+                    tsDict={tsDict}
+                    dispatches={dispatches}
+                    key={`tsd-${i}`} />;
+            });
+        // return (week.length < 7) ? this.renderIncompleteWeek(week) : this.renderCompleteWeek(week);
     }
 
     render() {
