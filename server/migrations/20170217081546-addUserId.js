944bf435e8e83befe87a0d4d86da9d9dc32ad5fd
Robert Schneiderman <robert.a.schneiderman@gmail.com>
2017-04-19 00:19:47 -0700

    commented out addUserId migration

--- server/migrations/20170217081546-addUserId.js
+++ server/migrations/20170217081546-addUserId.js
@@ -1,16 +1,16 @@
-'use strict';
+// 'use strict';
 
-module.exports = {
-  up: function (queryInterface, Sequelize) {
-    return queryInterface.addColumn(
-      'Histories',
-      'userId',
-      Sequelize.INTEGER
-    );    
-  },
+// module.exports = {
+//   up: function (queryInterface, Sequelize) {
+//     return queryInterface.addColumn(
+//       'Histories',
+//       'userId',
+//       Sequelize.INTEGER
+//     );    
+//   },
 
-  down: function (queryInterface, Sequelize) {
-    return queryInterface.removeColumn('Histories', 'userId');
-  }
-};
+//   down: function (queryInterface, Sequelize) {
+//     return queryInterface.removeColumn('Histories', 'userId');
+//   }
+// };
 
